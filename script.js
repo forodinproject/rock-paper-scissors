3615657c0b7dad91c383c1062448184ba4a9bb8b
forodinproject <112388027+forodinproject@users.noreply.github.com>
2023-11-20 18:54:36 +0530

    Add funtions and fix typos

---
+++ script.js
@@ -0,0 +1,20 @@
+function getComputerChoice() {
+
+
+}
+function checkPlayerSelection (playerSelection) {
+
+}
+
+function playRound(playerSelection, computerSelection ) {
+
+
+}
+
+function game() {
+
+}
+let playerSelection = prompt("Enter Rock paper or scirssors: ");
+playerSelection = playerSelection.toLowerCase();
+checkPlayerSelection(playerSelection);
+let computerSelection = getComputerChoice();
