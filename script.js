ac63918773a14f01c80b1b78fecb4c56a79052a9
forodinproject <112388027+forodinproject@users.noreply.github.com>
2023-11-21 21:52:37 +0530

    Start all over from clean slate

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 function getComputerChoice() {
-    let randomNumber = Math.floor((Math.random() * 10));
-    console.log(randomNumber);
-    if (randomNumber >= 0 && randomNumber <= 3)
-    {
-        return "rock";
-    }
-    if (randomNumber >=4 && randomNumber <= 6) 
-    {
-        return "paper";
-    }
-    if (randomNumber >=7 && randomNumber <=9)
-    {
-        return "scissors";
-    }
-
-
+    const choice = ["rock", "paper", "scissors"];
+    let randomIndex = Math.floor(Math.random()*choice.length);
+    console.log(`computerSelection is ${choice[randomIndex]} `)
+    return(choice[randomIndex]);
 }
-// function checkPlayerSelection() {
-//     let isChoiceWrong = true;
-//     do {
-//         let playerSelection = prompt("Enter Rock paper or scissors: ");
-//         playerSelection = playerSelection.toLowerCase();
-//     }
-//     while(isChoiceWrong === true) {
-//         if (playerSelection === rock || playerSelection === paper || playerSelection === scissors)
-//         {
-//             isChoiceWrong = false;
-//             return;
-//         }
-//         else {
-//             console.log("enter correct choice"); 
-//         }
-//     }
-//     return;
-// }
 
-function playRound(playerSelection, computerSelection ) {
+function playRound(playerSelection, computerSelection) {
+    if (checkWinner(playerSelection, computerSelection) === "tie") {
+        return "It is a Tie!";
+    }
 
+    if (checkWinner(playerSelection, computerSelection) === "player") {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    if (checkWinner(playerSelection, computerSelection) === "computer") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
 
 }
 
-function game() {
-
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "tie";
+    }
+    if ((playerSelection === "rock" && computerSelection === "scissors") 
+    || (playerSelection === "paper" && computerSelection === "rock")
+    || (playerSelection === "scissors" && computerSelection === "paper"))
+    {
+        return "player";
+    }
+    return "computer";
 }
 
-//checkPlayerSelection();
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+computerSelection = getComputerChoice();
+const playerSelection = "rock";
+console.log(`playerSelection is ${playerSelection}`)
+console.log(playRound(playerSelection, computerSelection));
