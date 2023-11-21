b2875a952b76062f63dcd052bae576d3106d1cf8
forodinproject <112388027+forodinproject@users.noreply.github.com>
2023-11-21 10:03:46 +0530

    Implement getComputerChoice()

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 10));
+    console.log(randomNumber);
+    if (randomNumber >= 0 && randomNumber <= 3)
+    {
+        return "rock";
+    }
+    if (randomNumber >=4 && randomNumber <= 6) 
+    {
+        return "paper";
+    }
+    if (randomNumber >=7 && randomNumber <=9)
+    {
+        return "scissors";
+    }
 
 
 }
-function checkPlayerSelection (playerSelection) {
-
-}
+// function checkPlayerSelection() {
+//     let isChoiceWrong = true;
+//     do {
+//         let playerSelection = prompt("Enter Rock paper or scissors: ");
+//         playerSelection = playerSelection.toLowerCase();
+//     }
+//     while(isChoiceWrong === true) {
+//         if (playerSelection === rock || playerSelection === paper || playerSelection === scissors)
+//         {
+//             isChoiceWrong = false;
+//             return;
+//         }
+//         else {
+//             console.log("enter correct choice"); 
+//         }
+//     }
+//     return;
+// }
 
 function playRound(playerSelection, computerSelection ) {
 
@@ -14,7 +43,7 @@ function playRound(playerSelection, computerSelection ) {
 function game() {
 
 }
-let playerSelection = prompt("Enter Rock paper or scirssors: ");
-playerSelection = playerSelection.toLowerCase();
-checkPlayerSelection(playerSelection);
+
+//checkPlayerSelection();
 let computerSelection = getComputerChoice();
+console.log(computerSelection);
